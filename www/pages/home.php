76f471cfd1133340c52e1d8dd814dd21d2dd5384
draconigen <draconigen@gmail.com>
2023-06-06 21:18:05 +0200

    docs: :memo: add quick start to home page

--- www/pages/home.php
+++ www/pages/home.php
@@ -1,2 +1,9 @@
 <h1><?= $this->current->title ?></h1>
+
+<strong>Deployment Quick Start:</strong>
+<ul>
+    <li>update <em>partners.json</em> and run <a href="updatepartners.php">updatepartners.php</a> to update / load banners</li>
+    <li>run <a href="?export">/?export</a> to deploy to <em><?= $this->config->staticOut->path ?></em></li>
+</ul>
+
 <?php debug($this->current) ?>
