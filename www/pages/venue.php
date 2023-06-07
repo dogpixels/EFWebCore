d2e54afc8ef132c14fc72a7c18e86b6af13e96d7
draconigen <draconigen@gmail.com>
2023-06-07 22:16:04 +0200

    chore: :rocket: update venue to CCH Hamburg

---
+++ www/pages/venue.php
@@ -0,0 +1,210 @@
+<h1>Our Venue &ndash; The Congress Center Hamburg</h1>
+<section class="uk-margin-bottom">
+	<div class="uk-column-1-2@l">
+		<p>In the center of Hamburg, Germany, amidst  lots of green and between Alster lake and a beatiful park to your leisure, lies the Congress Center Hamburg (CCH).</p>
+		<p>The Dammtor mainline train station and several other public transportation connections within walking distance allow for an easy travel.</p>
+		<p>Countless hotels, bars and restaurants around the neighborhood enrich the experience with a vast number of options to cater any taste and leave no desire unanswered.</p>
+		
+		<h2>Features</h2>
+		<p>These impressive numbers allow for growth beyond many years to come</p>
+
+		<ul>
+			<li><strong>50</strong> halls harbor up to <strong>12,000</strong> seats at once.</li>
+			<li>The largest theater, <span class="uk-text-italic">Saal 1</span>, sports <strong>3,025</strong> seats.</li>
+			<li><strong>36,000</strong> m² of total area to explore.</li>
+			<li>The nearby Hamburg Airport connects <strong>130</strong> destinations and can be reached within <strong>25</strong> minutes.</li>
+			<li><strong>40</strong> hotels in the vicinity offer up to <strong>7,900</strong> beds in total.</li>
+			<li>The adjacent "<span class="uk-text-italic">Planten un Blomen</span>" parks span <strong>47</strong> hectares.</li>
+		</ul>
+
+		<div class="uk-card uk-card-default uk-margin-bottom">
+			<div class="uk-card-media-top">
+				<div uk-lightbox>
+					<a href="img/pages/venue/Luftaufnahme_04.jpg"><img src="img/pages/venue/thumbs/Luftaufnahme_04.jpg" alt="CCH from above" /></a>
+				</div>
+			</div>
+			<div class="uk-card-body">
+				<img src="img/pages/venue/cch-logo-quer2_72dpi.jpg" alt="CCH - Congress Center Hamburg" />
+				<p>
+					Congressplatz 1<br />
+					20355 Hamburg<br />
+					Germany<br />
+					<a href="https://www.cch.de/en" target="_blank">www.cch.de/en</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<hr />
+
+<section>
+	<h2>Floor Maps</h2>
+	<div class="uk-grid-small uk-grid-match uk-child-width-1-2@l uk-child-width-1-4@xl uk-margin-bottom" uk-grid>
+		<div>
+			<div class="uk-card uk-card-default" uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-top">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-eg_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-eg_150dpi_en_2.jpg" alt="Ground Floor Map" /></a>
+					</div>
+				</div>
+				<div class="uk-card-body">
+					<h3 class="uk-card-title reset-font">Ground Floor</h3>
+					<ul>
+						<li>Entrance</li>
+						<li>Registration</li>
+						<li>More To Be Announced</li>
+					</ul>
+				</div>
+				<!-- <div class="uk-card-media-bottom">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-eg_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-eg_150dpi_en_2.jpg" alt="Ground Floor Map" /></a>
+					</div>
+				</div> -->
+			</div>
+		</div>
+		<div>
+			<div class="uk-card uk-card-default" uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-top">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-1og_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-1og_150dpi_en_2.jpg" alt="1st Floor Map" /></a>
+					</div>
+				</div>
+				<div class="uk-card-body">
+					<h3 class="uk-card-title reset-font">1st Floor</h3>
+					<ul>
+						<li>To Be Announced</li>
+					</ul>
+				</div>
+				<!-- <div class="uk-card-media-bottom">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-1og_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-1og_150dpi_en_2.jpg" alt="1st Floor Map" /></a>
+					</div>
+				</div> -->
+			</div>
+		</div>
+		<div>
+			<div class="uk-card uk-card-default" uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-top">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-2og_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-2og_150dpi_en_2.jpg" alt="2nd Floor Map" /></a>
+					</div>
+				</div>
+				<div class="uk-card-body">
+					<h3 class="uk-card-title reset-font">2nd Floor</h3>
+					<ul>
+						<li>To Be Announced</li>
+					</ul>
+				</div>
+				<!-- <div class="uk-card-media-bottom">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-2og_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-2og_150dpi_en_2.jpg" alt="2nd Floor Map" /></a>
+					</div>
+				</div> -->
+			</div>
+		</div>
+		<div>
+			<div class="uk-card uk-card-default" uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-top">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-3og_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-3og_150dpi_en_2.jpg" alt="3rd Floor Map" /></a>
+					</div>
+				</div>
+				<div class="uk-card-body">
+					<h3 class="uk-card-title reset-font">3rd Floor</h3>
+					<ul>
+						<li>To Be Announced</li>
+					</ul>
+				</div>
+				<!-- <div class="uk-card-media-bottom">
+					<div uk-lightbox>
+						<a href="img/pages/venue/cch_plan-iso-3og_150dpi_en.jpg"><img src="img/pages/venue/thumbs/cch_plan-iso-3og_150dpi_en_2.jpg" alt="3rd Floor Map" /></a>
+					</div>
+				</div> -->
+			</div>
+		</div>
+	</div>
+</section>
+
+<hr />
+
+<section>
+	<h2>Planten un Blomen, Gardens and Parks</h2>
+
+	<p>Curled around the CCH is <a href="https://plantenunblomen.hamburg.de/planten-un-blomen-parkplan-parkteile/" target="_blank">a cluster of parks</a> operated by the City of Hamburg, connecting the CCH all the way south to the <span class="uk-text-italic">Landungsbr&uuml;cken</span> docks with a 2.5 km walk in the greens.</p>
+	<p>There are several noteworthy points to mention:</p>
+	<ul>
+		<li>Entrance is free.</li>
+		<li><a href="https://plantenunblomen.hamburg.de/service/3834054/oeffnungszeiten-artikel/" target="_blank">Daily opening times: 7:00&mdash;23:00</a></li>
+		<li><a href="https://plantenunblomen.hamburg.de/essen-trinken/" target="_blank">There are several cafés, ice cream points and a restaurant.</a></li>
+		<li><a href="https://plantenunblomen.hamburg.de/veranstaltungskalender/" target="_blank">There may be events and shows taking place during Eurofurence.</a></li>
+	</ul>
+
+	<p>Check out <a href="https://plantenunblomen.hamburg.de/parkplan/" target="_blank">the official map of all points of interest</a>.</p>
+
+	<p><strong>Note:</strong> All this information may change at any time. </a href="https://plantenunblomen.hamburg.de/">Check out their website</a> and local signage for possible changes.</p>	
+</section>
+
+<hr />
+
+<section>
+	<h2>Official </h2>
+	<div class="uk-grid-small uk-grid-match uk-child-width-1-2@l uk-child-width-1-3@xl uk-margin-bottom" uk-grid>
+		<div>
+			<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-left uk-cover-container">
+					<a href="https://www.cch.de/en/visit/arrival" target="_blank" class="ef-hide-ext"><img src="img/pages/venue/thumbs/Luftaufnahme_01.jpg" alt="" uk-cover /></a>
+				</div>
+				<div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title no-font">Travel</h3>
+						<a href="https://www.cch.de/en/visit/arrival" target="_blank">Check out convenient descriptions on how to reach the congress center, no matter if you ride, drive or fly.</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-left uk-cover-container">
+					<a href="https://www.cch.de/en/visit/hotels" target="_blank" class="ef-hide-ext"><img src="img/pages/venue/thumbs/Taktiles_Bodenleitsystem.jpg" alt="" uk-cover /></a>
+				</div>
+				<div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title no-font">Hotels</h3>
+						<a href="https://www.cch.de/en/visit/hotels" target="_blank">The CCH presents seven hotels in the direct vicinity to the convention center.</a> Additionally, <a href="https://www.hamburg-travel.com/discover-hamburg/experience-sustainability/sustainable-accommodations/" target="_blank">the City of Hamburg provides a list of sustainable accomodations</a> for you to sleep in good consciousnes.
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<div>
+			<div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid uk-scrollspy="cls:uk-animation-fade">
+				<div class="uk-card-media-left uk-cover-container">
+					<a href="https://www.cch.de/en/venue/sustainability" target="_blank" class="ef-hide-ext"><img src="img/pages/venue/thumbs/Planten_un_Blomen_6.jpg" alt="" uk-cover /></a>
+				</div>
+				<div>
+					<div class="uk-card-body">
+						<h3 class="uk-card-title no-font">Sustainability</h3>
+						<a href="https://www.cch.de/en/venue/sustainability" target="_blank">Read about the CCH's commitment to responsibility and sustainability.</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<hr />
+
+<section class="uk-grid-small uk-flex-center uk-margin-bottom" uk-grid uk-lightbox>
+	<div><a href="img/pages/venue/CCH-Außenansicht_07.jpg" data-caption="Exterior View"><img src="img/pages/venue/thumbs/CCH-Außenansicht_07.jpg" alt="Exterior View" /></a></div>
+	<div><a href="img/pages/venue/Eingangshalle_des_CCH_1.jpg" data-caption="Entrance Hall"><img src="img/pages/venue/thumbs/Eingangshalle_des_CCH_1.jpg" alt="Entrance Hall" /></a></div>
+	<div><a href="img/pages/venue/Saal_1_3.jpg" data-caption="The largest theater, Saal 1"><img src="img/pages/venue/thumbs/Saal_1_3.jpg" alt="The largest theater, Saal 1" /></a></div>
+	<div><a href="img/pages/venue/CCH_4.jpg" data-caption=""><img src="img/pages/venue/thumbs/CCH_4.jpg" alt="" /></a></div>
+	<div><a href="img/pages/venue/Planten_un_Blomen_1.jpg" data-caption="The adjacent parks, Planten un Blomen"><img src="img/pages/venue/thumbs/Planten_un_Blomen_1.jpg" alt="The adjacent parks, Planten un Blomen" /></a></div>
+	<div><a href="img/pages/venue/Planten_un_Blomen_8.jpg" data-caption="The adjacent parks, Planten un Blomen"><img src="img/pages/venue/thumbs/Planten_un_Blomen_8.jpg" alt="The adjacent parks, Planten un Blomen" /></a></div>
+</section>
+
+<section class="uk-text-right">
+	<p>All images &copy; The City of Hamburg, Hamburg Messe respectively.</p>
+</section>
