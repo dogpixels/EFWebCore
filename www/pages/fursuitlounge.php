d2665d2c60049c2a3b3c3d9ac56dbbd1841a938b
draconigen <draconigen@gmail.com>
2024-06-13 12:43:24 +0200

    feat: :sparkles: add fursuitlounge page

---
+++ www/pages/fursuitlounge.php
@@ -0,0 +1,54 @@
+<h1>Fursuit Lounge Rules</h1>
+<div class="uk-grid-small uk-grid-match uk-child-width-1-3@m uk-margin-bottom" uk-grid>
+	<div>
+		<section>
+			<img src="img/pages/fursuitlounge/fsl_rules_fans.jpg" alt="Fursuit Lounge Rules: Fans" class="uk-margin uk-margin-right uk-float-left uk-width-1-2@xl" />
+			<p class="uk-margin-remove-top">Fans in the Fursuit Lounge should only be used for cooling fursuiters and not for drying fursuits, their parts, towels, or other things. Blocking or misusing fans can disrupt airflow and create safety hazards, potentially causing harm to both the equipment and the users.</p>
+			<p>Fans are strategically placed to maximize cooling efficiency and ensure a comfortable environment for fursuiters. Hanging or placing items on fans obstructs their function, reducing the overall cooling effect and potentially causing overheating issues. This rule ensures that fans remain unobstructed and effective for their intended purpose.</p>
+			<p>Moreover, hanging fursuit parts on fans can lead to serious safety concerns. Items could get sucked into the fan blades, causing damage to both the fan and the fursuit parts. Additionally, moisture and sweat from the suits can be blown onto other people, creating an unpleasant and unsanitary situation. By regulating the use of fans, we maintain a safe, clean, and efficient lounge environment for all fursuiters.</p>
+		</section>
+	</div>
+	<div>
+		<section>
+			<img src="img/pages/fursuitlounge/fsl_rules_nohardcases.jpg" alt="Fursuit Lounge Rules: No Hard Cases" class="uk-margin uk-margin-right uk-float-left uk-width-1-2@xl" />
+			<p class="uk-margin-remove-top">Hard fursuit cases, which retain their shape even when empty, are not allowed in the Fursuit Lounge due to the general lack of space. The Fursuit Lounge is intended as a relaxation and changing area for over 2500 fursuiters, not as a storage area. Hard cases would significantly reduce the available space for fursuiters to comfortably change and relax, thus defeating the core purpose of the Fursuit Lounge.</p>
+			<p>The Fursuit Lounge prioritizes maximizing usable space for the comfort and convenience of the fursuiters. Hard cases occupy more room and cannot be easily tucked away or compressed. This can cause hazards of all kinds, from tripping over them to hard cases that are stacked falling down on people, thus making it difficult to use the area safely and efficiently.</p>
+			<p>This rule ensures that the space remains open and accessible for everyone. Allowing hundreds of hard cases would necessitate strict limits on the number of people who could use the lounge at any given time. This could lead to frustration and inconvenience, especially during peak hours when many fursuiters need to use the facilities. By banning hard cases outright, we maintain an optimal balance between accessibility and comfort for all users.</p>
+		</section>
+	</div>
+	<div>
+		<section>
+			<img src="img/pages/fursuitlounge/fsl_rules_nophotos.jpg" alt="Fursuit Lounge Rules: No Photography or Video Recording" class="uk-margin uk-margin-right uk-float-left uk-width-1-2@xl" />
+			<p class="uk-margin-remove-top">Photography and video recording are absolutely forbidden in the Fursuit Lounge, regardless of whether they are aimed away from other people or not. This rule is in place to protect the privacy and comfort of individuals who are changing in the room. The lounge is a space where fursuiters can relax and change without worrying about being recorded or photographed.</p>
+			<p>Respecting privacy is crucial in maintaining a safe and welcoming environment for all fursuiters. Many individuals might feel vulnerable while changing, and the presence of cameras can exacerbate this feeling. Ensuring that the lounge is a camera-free zone allows fursuiters to relax and enjoy their time without the concern of unintentional exposure.</p>
+			<p>The potential for accidental inclusion of other individuals in the background of photos or videos poses a significant privacy risk. Even well-intentioned recordings aimed away from people can inadvertently capture reflections or shadows. By enforcing a strict no-photography policy, we eliminate the risk of privacy breaches and uphold the trust and comfort of all lounge users.</p>
+		</section>
+	</div>
+</div>
+<hr style="clear:both" />
+<div class="uk-grid-small uk-grid-match uk-child-width-1-3@m uk-margin-bottom" uk-grid>
+	<div>
+		<section>
+			<img src="img/pages/fursuitlounge/fsl_rules_noglass.jpg" alt="Fursuit Lounge Rules: No Glass" class="uk-margin uk-margin-right uk-float-left uk-width-1-2@xl" />
+			<p class="uk-margin-remove-top">Glass items, such as bottles and drinking glasses, are prohibited in the Fursuit Lounge due to the high risk of shards potentially injuring barefoot fursuiters or damage fursuits. The rule is put in place to prevent accidents and ensure the safety of everyone in the lounge.</p>
+			<p>The lounge is a space where fursuiters often change quickly, sometimes stepping out of their suits and walking barefoot. Broken glass poses a significant hazard in such an environment, as it can easily cause cuts and injuries. The risk is not worth the convenience of having glass items, hence their strict prohibition.</p>
+			<p>Past incidents of broken glass in the lounge have highlighted the need for this rule. Cleaning up glass shards is difficult and time-consuming, and missing even a small piece can lead to serious injuries. By banning glass items altogether, we ensure a safer, cleaner environment where fursuiters can relax without fear of injury.</p>
+		</section>
+	</div>
+	<div>
+		<section>
+			<img src="img/pages/fursuitlounge/fsl_rules_nofood.jpg" alt="Fursuit Lounge Rules: No Outside Food" class="uk-margin uk-margin-right uk-float-left uk-width-1-2@xl" />
+			<p class="uk-margin-remove-top">Outside food is not allowed in the Fursuit Lounge due to the potential mess and damage it can cause to fursuits. Messy or fatty foods like ice cream, fries, and hamburgers are particularly problematic as they can leave stains and residues on the suits, which are often difficult or impossible to clean.</p>
+			<p>Fursuits are significant investments, both financially and emotionally, for many participants. Protecting them from accidental damage is a top priority. Food spills and grease stains can permanently damage the material, causing discoloration or deterioration that diminishes the suit's appearance and lifespan. By prohibiting outside food, we minimize the risk of such damage.</p>
+			<p>Moreover, the lounge needs to remain a clean and pleasant environment for all users. Food packing and residues can create unpleasant odors, detracting from the overall experience. By keeping the lounge free from outside food, we maintain a hygienic and welcoming space conducive to relaxation and enjoyment. And remember: we always serve small snacks to get your blood sugar back on line.</p>
+		</section>
+	</div>
+	<div>
+		<section>
+			<img src="img/pages/fursuitlounge/fsl_rules_nopaint.jpg" alt="Fursuit Lounge Rules: No Paint or Hazardous Items" class="uk-margin uk-margin-right uk-float-left uk-width-1-2@xl" />
+			<p class="uk-margin-remove-top">Paint, including face paint, is not allowed in the Fursuit Lounge because it can transfer onto fursuits and cause damage. Additionally, fire, sharp objects, and other hazardous items are prohibited for obvious safety reasons. These restrictions are vital for protecting both the fursuits and the fursuiters.</p>
+			<p>Paint can easily rub off onto fursuits, leading to stains that are often difficult to remove. This can ruin the appearance of a suit and potentially require expensive professional cleaning or repairs. By banning paint, we prevent such accidental transfers and ensure that fursuits remain in pristine condition.</p>
+			<p>Safety is a paramount concern in the Fursuit Lounge. Fire and sharp objects pose significant risks in a crowded, enclosed space where people are often distracted while changing or relaxing. Preventing the introduction of such items reduces the likelihood of accidents and injuries, creating a safer environment for everyone.</p>
+		</section>
+	</div>
+</div>
