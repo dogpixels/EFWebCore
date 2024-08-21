ea62cc52df8567d7ecc2aac7908739412bc78017
draconigen <draconigen@dogpixels.net>
2024-08-21 13:24:35 +0200

    feat: :sparkles: job deep-linking

--- www/js/main.js
+++ www/js/main.js
@@ -2,3 +2,20 @@
 document.getElementById("nav-state").addEventListener('change', function() {
     document.getElementsByTagName("body")[0].style = "overflow: " + (this.checked? "hidden" : "auto") + ";"
 })
+
+// job deeplinking
+document.querySelectorAll('[id^=ef-job-]').forEach(element => {
+    // attach hash on open
+    UIkit.util.on(element, 'beforeshow', () => {
+        window.location.hash = element.id.substring(7);
+    });
+    // remove hash on close
+    UIkit.util.on(element, 'beforehide', () => {
+        window.location.hash = '';
+    });
+});
+// open job on page load
+const job = document.getElementById(`ef-job-${window.location.hash.substring(1)}`);
+if (window.location.hash && job) {
+    UIkit.modal(job).show();
+}
